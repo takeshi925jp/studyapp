5fdfe18bab3d0791480a7c66283877e33e67662a
github takeshi925jp <takeshi925jp@gmail.com>
2022-08-27 10:58:05 +0900

    コミット１

--- frontend/src/components/modules/Title.js
+++ frontend/src/components/modules/Title.js
@@ -1,22 +1,30 @@
 import React from "react";
 import { useState, useEffect } from 'react'
+import Grid from '@mui/material/Grid'; // Grid version 1
+import Grid2 from '@mui/material/Unstable_Grid2'; // Grid version 2
 
 const Title = (props) => {
 
     const [title, setTitle] = useState("");
+    const [returnFlg, setReturnFlg] = useState(false);
 
     return (
         <>
-            <div className="title-area">
-                
-                <p>{props.title}</p>                
-                <div>
+            <Grid container spacing={2}>
+                <Grid xs={8}>
+                    <p>{props.title}</p>
+                </Grid>
+                <Grid xs={1}>
                     <a href="/login">ログアウト</a>
-                </div>
-                <div>
-                    <a href={props.return_link}>戻る</a>
-                </div>
-            </div>
+                </Grid>
+                <Grid xs={1}>
+                {props.returnFlg && (
+                    <div>
+                        <a href={props.return_link}>戻る</a>
+                    </div>
+                )}
+                </Grid>
+            </Grid>
         </>
     );
 };
--- frontend/src/components/pages/StudySchedule.js
+++ frontend/src/components/pages/StudySchedule.js
@@ -1,14 +1,16 @@
 import Header from '../modules/Header'
 import Footer from '../modules/Footer'
 import Title from '../modules/Title'
+import IconMenu from '../modules/IconMenu'
 
 export const StudySchedule = () => {
 
-  const info = { title: '学習予定一覧', return_link: ''}
+  const info = { title: '学習予定一覧', returnFlg: false}
   return (
     <>
       <Header />
       <Title {...info}/>
+      <IconMenu />
       <Footer />
     </>
   );
